075c8cdfe22d627edac87222de4ad7317fd688a6
Nishchal370 <nishchal.370@gmail.com>
2022-01-07 14:12:19 +0545

    product, billProduct, orderProduct model created and registered to admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin.sites import site
 
-from .models import Bill, Customer, Order
+from .models import Bill, BillProduct, Customer, Order, OrderProduct, Product
 
 class CustomerDisplay(admin.ModelAdmin):
     list_display = ('customerId', 'name', 'address', 'phone', 'email')
@@ -9,9 +10,21 @@ class OrderDisplay(admin.ModelAdmin):
     list_display = ('orderId', 'customerId', 'date', 'rate', 'advanceAmount', 'submittionDate', 'submittedDate', 'design', 'status', 'remark')
 
 class BillDisplay(admin.ModelAdmin):
-    list_display= ('billId', 'customerId', 'orderId', 'date', 'rate', 'customerProductWeight', 'customerProductAmount', 'totalAmount', 'discount', 'grandTotalAmount', 'advanceAmount', 'payedAmount', 'remainingAmount', 'status')
+    list_display = ('billId', 'customerId', 'orderId', 'date', 'rate', 'customerProductWeight', 'customerProductAmount', 'totalAmount', 'discount', 'grandTotalAmount', 'advanceAmount', 'payedAmount', 'remainingAmount', 'status')
+
+class ProductDisplay(admin.ModelAdmin):
+    list_display = ('productId', 'productName', 'netWeight', 'size', 'gemsName', 'gemsPrice')
+
+class BillProductDisplay(admin.ModelAdmin):
+    list_display = ('billProductId', 'billId', 'productId', 'lossWeight', 'totalWeight', 'rate', 'makingCharge', 'totalAmountPerProduct')
+
+class OrderProductDisplay(admin.ModelAdmin):
+    list_display = ('orderProductId', 'orderId', 'productId', 'totalWeight', 'status')
 
 # Register your models here.
 admin.site.register(Customer, CustomerDisplay)
-admin.site.register(Order, OrderDisplay)
 admin.site.register(Bill, BillDisplay)
+admin.site.register(Order, OrderDisplay)
+admin.site.register(Product, ProductDisplay)
+admin.site.register(BillProduct, BillProductDisplay)
+admin.site.register(OrderProduct, OrderProductDisplay)
--- main/models.py
+++ main/models.py
@@ -38,7 +38,7 @@ class Bill(models.Model):
     ]
 
     billId =  models.AutoField(primary_key=True, null=False)
-    orderId = models.ForeignKey(Order, null=True, on_delete=CASCADE, related_name='orders')
+    orderId = models.ForeignKey(Order, null=True, blank=True, on_delete=CASCADE, related_name='orders')
     customerId = models.ForeignKey(Customer, null=True, on_delete=CASCADE, related_name='bills')
     date = models.DateField(null=False, default=now)
     rate = models.FloatField(null=True, blank=True)
@@ -57,3 +57,42 @@ class Bill(models.Model):
 
 
 
+class Product(models.Model):
+    productId = models.AutoField(primary_key=True, null=False)
+    productName = models.CharField(max_length=50, null=False)
+    netWeight = models.FloatField()
+    size = models.FloatField()
+    gemsName = models.CharField(max_length=50, null=True, blank=True)
+    gemsPrice = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.productId}'
+
+
+class BillProduct(models.Model):
+    billProductId = models.AutoField(primary_key=True, null=False)
+    billId = models.ForeignKey(Bill, null=False, on_delete=CASCADE, related_name='bill')
+    productId = models.ForeignKey(Product, null=False, on_delete=CASCADE, related_name='product')
+    lossWeight = models.FloatField(null=True, blank=True)
+    totalWeight = models.FloatField(null=True, blank=True)
+    rate = models.FloatField(null=True, blank=True)
+    makingCharge = models.FloatField(null=True, blank=True)
+    totalAmountPerProduct = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.billProductId}'
+
+
+class OrderProduct(models.Model):
+    order_status = [
+        ('P','Pending'),
+        ('S','Submitted'),
+    ]
+    orderProductId = models.AutoField(primary_key=True, null=False)
+    orderId = models.ForeignKey(Order, null=False, on_delete=CASCADE, related_name='order')
+    productId = models.ForeignKey(Product, null=False, on_delete=CASCADE, related_name='orderProducts')
+    totalWeight = models.FloatField(null=True, blank=True)
+    status = models.CharField(max_length=11, null=False, choices=order_status, default='Pending')
+
+    def __str__(self):
+        return f'{self.orderProductId}'
